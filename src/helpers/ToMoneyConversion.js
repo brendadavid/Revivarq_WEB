2650626efe78b6a1a00dd3ba7683581ae930dab6
Brenda David <brenda.david@acad.pucrs.br>
2022-06-19 18:55:18 -0300

    fix: ajusta code smell minor - 'substr' is deprecated. A legacy feature for browser compatibility

--- src/helpers/ToMoneyConversion.js
+++ src/helpers/ToMoneyConversion.js
@@ -8,7 +8,7 @@ const toMoneyConversion = (currency, digits, dot, transform) => {
     i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c)));
     j = (j = i.length) > 3 ? j % 3 : 0;
 
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    return s + (j ? i.slice(0, j) + t : "") + i.slice(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 
 export default toMoneyConversion;
